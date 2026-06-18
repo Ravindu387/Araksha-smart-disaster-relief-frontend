317599326d71ece3416dcfc8b56c3dc1e925da8a
Dulith Thenuka <duliththenuka912@gmail.com>
2026-06-18 13:13:25 +0530

    Created Dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { Sidebar } from './Admin/Componet/layouts/sidebar/sidebar';
+import { Header } from './Admin/Componet/layouts/header/header';
+import { Dashboard } from './Admin/Componet/pages/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    Sidebar,
+    Header,
+    Dashboard
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Araksha-smart-disaster-relief-frontend');
-}
+export class App {}
