a85aceca67ba0956a3ba5a0bb21efc50af3094d2
Dulith Thenuka <duliththenuka912@gmail.com>
2026-06-23 13:04:17 +0530

    Delete conponet

--- src/app/Admin/Componet/pages/live-tracking/live-tracking.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-live-tracking',
-  imports: [],
-  templateUrl: './live-tracking.html',
-  styleUrl: './live-tracking.css',
-})
-export class LiveTracking {}
