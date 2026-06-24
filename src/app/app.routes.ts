7dce29115e585f69d6a6d815b9e400233c1a7267
Lakshitha Dilshan Subhasingha <dlakshitha812@gmail.com>
2026-06-25 03:27:04 +0530

    create inventory

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { Dashboard } from './Admin/Componet/pages/dashboard/dashboard';
 //import { EmergencyRequests } from './Admin/Componet/pages/emergency-requests/emergency-requests';
 //import { Volunteers } from './Admin/Componet/pages/volunteers/volunteers';
 //import { Shelters } from './Admin/Componet/pages/shelters/shelters';
-//import { Inventory } from './Admin/Componet/pages/inventory/inventory';
+import { Inventory } from './Admin/Componet/pages/inventory/inventory';
 //import { Allocation } from './Admin/Componet/pages/allocation/allocation';
 //import { LiveTracking } from './Admin/Componet/pages/live-tracking/live-tracking';
 //import { Notifications } from './Admin/Componet/pages/notifications/notifications';
@@ -28,7 +28,7 @@ export const routes: Routes = [
       // { path: 'emergency-requests', component: EmergencyRequests },
       // { path: 'volunteers', component: Volunteers },
       // { path: 'shelters', component: Shelters },
-      // { path: 'inventory', component: Inventory },
+      { path: 'inventory', component: Inventory },
       // { path: 'allocation', component: Allocation },
       // { path: 'live-tracking', component: LiveTracking },
       // { path: 'notifications', component: Notifications },
