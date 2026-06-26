9e24e62502dfe324b97e5561eb3ccd6909d79151
Lakshitha Dilshan Subhasingha <dlakshitha812@gmail.com>
2026-06-27 02:04:06 +0530

    updated route file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,52 @@
 import { Routes } from '@angular/router';
 
 import { LandingPage } from './Common/landing-page/landing-page';
+import { Login } from './Common/login/login';
+import { SignupComponent } from './Common/signup/signup';
 
 import { AdminLayout } from './Admin/Componet/layouts/admin-layout/admin-layout';
 
 import { Dashboard } from './Admin/Componet/pages/dashboard/dashboard';
-//import { EmergencyRequests } from './Admin/Componet/pages/emergency-requests/emergency-requests';
-//import { Volunteers } from './Admin/Componet/pages/volunteers/volunteers';
-//import { Shelters } from './Admin/Componet/pages/shelters/shelters';
+import { EmergencyRequestsComponent } from './Admin/Componet/pages/emergency-requests.component/emergency-requests.component';
+import { VolunteersComponent } from './Admin/Componet/pages/volunteers.component/volunteers.component';
+import { Shelters } from './Admin/Componet/pages/shelters/shelters';
 import { Inventory } from './Admin/Componet/pages/inventory/inventory';
-//import { Allocation } from './Admin/Componet/pages/allocation/allocation';
-//import { LiveTracking } from './Admin/Componet/pages/live-tracking/live-tracking';
-//import { Notifications } from './Admin/Componet/pages/notifications/notifications';
-//import { Reports } from './Admin/Componet/pages/reports/reports';
+import { AllocationComponent } from './Admin/Componet/pages/allocation/allocation';
+import { NotificationsComponent } from './Admin/Componet/pages/notifications/notification';
+//import { ReportsComponent, LiveTrackingComponent } from './Admin/Componet/pages/reports/reports';
 import { Settings } from './Admin/Componet/pages/settings/settings';
+
+import { Layout as CitizenLayout } from './Citizen/Component/Layout/layout/layout';
+import { CitizenDashboard } from './Citizen/Component/Pages/Dashboard/citizen-dashboard/citizen-dashboard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'LandingPage', pathMatch: 'full' },
 
   { path: 'LandingPage', component: LandingPage },
-  
-{
+  { path: 'login', component: Login },
+  { path: 'signup', component: SignupComponent },
+
+  {
+    path: 'citizen',
+    component: CitizenLayout,
+    children: [
+      { path: 'dashboard', component: CitizenDashboard },
+    ],
+  },
+
+  {
     path: '',
     component: AdminLayout,
     children: [
       { path: 'dashboard', component: Dashboard },
-      // { path: 'emergency-requests', component: EmergencyRequests },
-      // { path: 'volunteers', component: Volunteers },
-      // { path: 'shelters', component: Shelters },
+      { path: 'emergency-requests', component: EmergencyRequestsComponent },
+      { path: 'volunteers', component: VolunteersComponent },
+      { path: 'shelters', component: Shelters },
       { path: 'inventory', component: Inventory },
-      // { path: 'allocation', component: Allocation },
-      // { path: 'live-tracking', component: LiveTracking },
-      // { path: 'notifications', component: Notifications },
-      // { path: 'reports', component: Reports },
+      { path: 'allocation', component: AllocationComponent },
+     // { path: 'live-tracking', component: LiveTrackingComponent },
+      { path: 'notifications', component: NotificationsComponent },
+      //{ path: 'reports', component: ReportsComponent },
       { path: 'settings', component: Settings },
     ],
   },
