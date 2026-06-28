ea85ccd538683a387ea70941d67d7eacead33b8a
Lithira <lithiramandula1@gmail.com>
2026-06-29 00:24:26 +0530

    create citizen

--- src/app/Citizen/Component/Pages/Dashboard/citizen-dashboard/citizen-dashboard.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-citizen-dashboard',
-  imports: [],
-  templateUrl: './citizen-dashboard.html',
-  styleUrl: './citizen-dashboard.css',
-})
-export class CitizenDashboard {}
