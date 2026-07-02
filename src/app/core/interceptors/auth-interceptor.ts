b2e008b33404ca24a7bef003f6fb6f5a34d7b396
Lakshitha Dilshan Subhasingha <dlakshitha812@gmail.com>
2026-07-03 02:19:22 +0530

    update intercepter

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -4,12 +4,16 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   const token = localStorage.getItem('token');
 
+  console.log('Interceptor Token:', token);
+
   if (token) {
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
+
+    console.log('Authorization Header Added');
   }
 
   return next(req);
